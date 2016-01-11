736d5b7f9e107ca747fb1c53e87de88f29c71f20
Leif-Jöran Olsson <ljo@fripost.org>
2016-01-11 16:50:32 +0100

    Small cleanup of imports.

--- java/src/org/exist/indexing/rdf/TDBRDFIndex.java
+++ java/src/org/exist/indexing/rdf/TDBRDFIndex.java
@@ -1,7 +1,5 @@
 package org.exist.indexing.rdf;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.DirectoryIteratorException;
